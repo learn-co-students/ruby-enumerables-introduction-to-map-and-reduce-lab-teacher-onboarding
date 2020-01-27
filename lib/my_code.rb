9f34a154daa760ddbb48f14c8abf280c3757d1be
Marc Majcher <majcher@gmail.com>
2020-01-27 11:12:13 -0600

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
 # My Code here....
+def map_to_negativize(arr)
+  rarr = []
+  arr.each { |e| rarr.push(e * -1) }
+  rarr
+end
+
+def map_to_no_change(arr)
+  arr
+end
+
+def map_to_double(arr)
+  rarr = []
+  arr.each { |e| rarr.push(e * 2) }
+  rarr
+end
+
+def map_to_square(arr)
+  rarr = []
+  arr.each { |e| rarr.push(e ** 2) }
+  rarr
+end
+
+def reduce_to_total(arr, start = 0)
+  total = start
+  arr.each { |e| total += e }
+  total
+end
+
+def reduce_to_all_true(arr)
+  result = true
+  arr.each { |e| result &&= !!e }
+  result
+end
+
+def reduce_to_any_true(arr)
+  result = false
+  arr.each { |e| result ||= !!e }
+  result
+end
